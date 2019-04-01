9e0f26eb21870fa1a589a0af7826a48057a363be
pumbf <892862286@qq.com>
2019-04-01 15:07:16 +0800

    add dao and pojo by mybaitis plugin

---
+++ src/main/java/me/pumbf/mmall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package me.pumbf.mmall.dao;
+
+import me.pumbf.mmall.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
